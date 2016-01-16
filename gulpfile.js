2d6c2dc35c49eae900f4fed218942e5b5f4fda02
sisisin <azsisisin@gmail.com>
2016-01-17 00:36:33 +0900

    add tweet func

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 const gulp = require('gulp');
 const browserify = require('browserify');
+const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 
 gulp.task('browserify', () => {
   return browserify({ entries: ['./src/js/renderer/index.js']})
+    .transform(babelify, { presets: ['es2015']})
     .bundle()
     .pipe(source('dist/bundle.js'))
     .pipe(gulp.dest('./'));
--- src/js/renderer/index.js
+++ src/js/renderer/index.js
@@ -1,1 +1,17 @@
-document.write('hoge');
+const Twit = require('twit');
+const T = new Twit(require('./settings'));
+
+document.querySelector('.twform')
+  .addEventListener('submit', (e) => {
+    e.preventDefault();
+    const tw = document.querySelector('.tweet');
+    const hs = document.querySelector('.hashtag');
+    let twString = tw.value;
+
+    if (hs.value !== '') twString += ` #${hs.value}`;
+
+    T.post('statuses/update', { status: twString }, (err, data, res) => {
+        if (err) alert(err.toString());
+        tw.value = '';
+      });
+});
